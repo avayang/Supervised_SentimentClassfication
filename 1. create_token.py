1247510471162b6de0faf433bdb0e054efa35136
avayang <ty2418@columbia.edu>
2020-06-06 16:16:02 -0400

    Add existing file

--- 1. create_token.py
+++ 1. create_token.py
@@ -1,3 +1,16 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import time
+import os
+import re
+from itertools import chain
+from transformers import BertTokenizer
+PRETRAINED_MODEL_NAME = "bert-base-uncased" #英文pretrain(不區分大小寫)
+print(torch.__version__)
+#1.3.1
+
 # load tokenizer
 tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)
 vocab = tokenizer.vocab
